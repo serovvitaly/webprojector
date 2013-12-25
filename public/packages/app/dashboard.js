3c2ce30ce3bdf292e748d2b11cf4c3f0bc8ecd10
Vitaly Serov <serovvitaly@gmail.com>
2013-12-25 20:54:42 +0400

    создано приложение Ember для dashboard

--- public/packages/app/dashboard.js
+++ public/packages/app/dashboard.js
@@ -1,5 +1,18 @@
 Dashboard = Ember.Application.create();
 
+Dashboard.ProjectController = Ember.Controller.extend({ 
+    actions: {
+        createProject: function(){
+            
+            var project = this.store.createRecord('prokat', {
+                title: 'Первый проект',
+                user: 'Foo Bar'
+            });
+            
+        }
+    } 
+});
+
 Dashboard.Router.map(function() {
   // put your routes here
 });
