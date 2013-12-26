51e6808526d0d45ba2b6472c1e244f1437721e6a
Vitaly Serov <serovvitaly@gmail.com>
2013-12-26 14:11:57 +0400

    работа с миграциями

---
+++ app/database/migrations/2013_12_26_090802_create_projects_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('projects', function($table){
+            $table->increments('id');
+            $table->string('title');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
