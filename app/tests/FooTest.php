627a9999ec6d542d49362ce38a36982edbe172da
Vitaly Serov <serovvitaly@gmail.com>
2013-12-27 19:48:38 +0400

    phpunit

---
+++ app/tests/FooTest.php
@@ -0,0 +1,15 @@
+<?php
+  
+class FooTest extends TestCase {
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testHelloWorld()
+    {
+        //$crawler = $this->client->request('GET', '/');
+
+        $this->assertTrue( false );
+    }
+}
