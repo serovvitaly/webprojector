c3747e99bcd7a95978dd6d1ff1f6c53259f2050f
Vitaly Serov <serovvitaly@gmail.com>
2013-12-29 06:38:50 +0400

    коммит

--- public/app/dashboard.js
+++ public/app/dashboard.js
@@ -4,4 +4,45 @@
 * @version 1.0.0
 * @author Vitaly Serov <serovvitaly@gmail.com>
 */
-Dashboard = '';
+
+var requireds = [
+    'packages/backbone/underscore-min',
+    'packages/backbone/backbone',
+    'packages/marionette/lib/backbone.marionette'
+];
+
+require(requireds, function(){
+    
+    DashboardTopSidebar = Marionette.ItemView.extend({
+        template: '#tpl-dashboard-top-sidebar'
+    });
+
+    DashboardBaseLayout = Marionette.Layout.extend({
+        el: 'body',
+        template: '#tpl-base-layout',
+        regions: {
+            topSidebar:  '#dashboard-top-sidebar',
+            baseSidebar: '#dashboard-base-sidebar',
+            context:     '#dashboard-context'
+        }
+    });
+
+    Dashboard = new Marionette.Application();
+
+    Dashboard.addInitializer(function(){
+        
+        this.layout = new DashboardBaseLayout();
+        this.layout.render();
+        
+        //this.layout.topSidebar.show( new DashboardTopSidebar() );
+        
+        this.w = new DashboardTopSidebar();
+        this.w.render();
+        
+        
+        
+        //builtLayout();
+    });
+
+    Dashboard.start(); 
+});
